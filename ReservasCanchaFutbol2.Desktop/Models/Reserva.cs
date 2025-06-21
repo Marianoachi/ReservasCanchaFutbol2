66de292b08118d87a1b6c2b75d5bbd918bc7cf91
Mariano Achi <achimariano@gmail.com>
2025-06-20 21:54:03 -0300

    add. creo la carepta reserva en winforms

---
+++ ReservasCanchaFutbol2.Desktop/Models/Reserva.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReservasCanchaFutbol2.Desktop.Models
+{
+    public class Reserva
+    {
+        public int Id { get; set; }
+        public int CanchaId { get; set; }
+        public string ClienteNombre { get; set; }
+        public DateTime FechaHora { get; set; }
+        public int DuracionHoras { get; set; }
+    }
+
+}
